6502aaf667cb9e8ffd341e7c541d3280a7cc2166
Ollie Turnbull <oturnbull1@gmail.com>
2021-10-26 16:27:39 +0100

    Added DicomReader to protoype

--- ciqc/prototype.py
+++ ciqc/prototype.py
@@ -1,22 +1,19 @@
 import cv2
 import os
 import pytesseract
-from pydicom import dcmread
 from remove_text import draw_boxes_on_text
+from DicomReader import DicomReader
+from pathlib import Path
 
 # Read in DICOM
-data_dir = os.path.abspath("test-dicoms")
-path = os.path.join(data_dir, "ultrasound1.dcm")
-dicom = dcmread(path)
-img = dicom.pixel_array
+data_reader = DicomReader("ultrasound1.dcm", "test-dicoms")
+data_reader.show_image()
 
-cv2.imshow("Output", img)
-cv2.waitKey(0)
 
 # Demo Text Blocking
 if os.name == 'nt':
     pytesseract.pytesseract.tesseract_cmd = "C:\\Program Files\\Tesseract-OCR\\tesseract.exe"
 
-img_boxes = draw_boxes_on_text(img)
+img_boxes = draw_boxes_on_text(data_reader.pixel_array)
 cv2.imshow("Output", img_boxes)
 cv2.waitKey(0)
