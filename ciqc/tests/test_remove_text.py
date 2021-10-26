3170377d22fa007b75c52f6f01990589eef5d2cb
Elliot <e.barbeary@outlook.com>
2021-10-26 16:42:40 +0100

    Update Unit Test Workflow

--- ciqc/tests/test_remove_text.py
+++ ciqc/tests/test_remove_text.py
@@ -9,12 +9,12 @@ from ciqc.remove_text import draw_boxes_on_text
 class TwoCompartmentModelTest(unittest.TestCase):
 
     def test_draw_boxes_on_text(self):
-        data_dir = os.path.abspath("test-dicoms")
+        data_dir = os.path.abspath(os.path.join("test-dicoms"))
         path = os.path.join(data_dir, "ultrasound1.dcm")
         dicom = dcmread(path)
         img = dicom.pixel_array
 
-        test_img = cv2.imread(os.path.join("ciqc", "tests", "test_boxes_img.png"))
+        test_img = cv2.imread(os.path.join("ciqc", "tests", "test_text_removal.png"))
         box_image = draw_boxes_on_text(img)
 
         np.testing.assert_equal(box_image, test_img)
